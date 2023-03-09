3fbec5a0454368bc1ec5f4ff84f5b7667ed61e06
LETAIHOANG <hoangle9989n@gmail.com>
2023-03-09 20:43:38 +0900

    [V2.0][Thực hành] 2. Sử dụng toán tử
    Thực hành tính diện tích hình tròn

---
+++ main.js
@@ -0,0 +1,9 @@
+
+
+
+    // let inputWidth;
+    // let inputHeigth;
+    // inputWidth = +prompt('Enter the width');
+    // inputHeigth = +prompt('Enter the heigth')
+    // let area = inputWidth*inputHeigth;
+    // document.write('The area is: ' + area);
